e10460ad34f91f42f52ae6f0b398a1ca9de1ee44
FabulousSpaceCat <christinamdeming@gmail.com>
2020-07-17 23:20:31 -0400

    Did a lot of styling, added some ... I dunno, instructions, in the page itself, cleaned up the JS a little.  Still need to run function when enter in text box.

--- tunes.js
+++ tunes.js
@@ -1,25 +1,38 @@
 
 let songs = [];
 
-function songRequest() {
-    songs.push(document.getElementById("request").value);
-    text = "<ul id=\"songTitle\">";
-    for (i = 0; i < songs.length; i++) {
-        text += "<li id=\"" + i + "\">" + songs[i] + "<span><a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\">&#9658;</a></span></li>";
+function listGen() {
+    var text = "<ul>";
+    for (let i = 0; i < songs.length; i++) {
+        text += "<a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\"><li id=\"" + i + "\">" + songs[i] + "<span>&#9658;</span></li></a>";
     }
     text += "</ul>";
-    document.getElementById("songlist").innerHTML = text;
+    document.getElementById("songList").innerHTML = text;
+}
+
+function songRequest() {
+    songs.push(document.getElementById("request").value);
+    listGen();
     document.getElementById("request").value = "";
 }
 
 function songPlayed(id) {
-    i = id.parentNode.parentNode.id;
+    let i = id.parentNode.parentNode.id;
     songs.splice(i,1);
-    text = "<ul class=\"songTitle\">";
-    for (i = 0; i < songs.length; i++) {
-        text += "<li id=\"" + i + "\">" + songs[i] + "<span><a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\">&#9658;</a></span></li>";
-    }
-    text += "</ul>";
-    document.getElementById("songlist").innerHTML = text;
+    listGen();    
 }
+
+/**
+ * Check into how this works, from Lothus: https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/indexof
+ * It will avoid rebuilding the ul every time, and maybe give me the ability to style the removal of the element with an animation or something.
+ * 
+ * function songPlayed(id) {
+ * var listItem = id.parentNode.parentNode;
+ * var listRoot = listItem.parentNode;
+ * var i = Array.prototype.indexOf.call(listRoot.children, listItem);
+ * songs.splice(i,1);
+ * listRoot.removeChild(listItem);
+ * };
+ * 
+ */
 
