5c08d7a2ad16b726087fe08b7b6e072812b33678
FabulousSpaceCat <christinamdeming@gmail.com>
2020-07-17 20:41:52 -0400

    It adds to the array which generates the ul, and it will remove specific list items when you play them both from the ul and the array.  It'll also clear the text box when you add a new item.  todo: run songRequest when you press enter in the text box.

--- tunes.js
+++ tunes.js
@@ -1,2 +1,25 @@
-let songs = []
+
+let songs = [];
+
+function songRequest() {
+    songs.push(document.getElementById("request").value);
+    text = "<ul id=\"songTitle\">";
+    for (i = 0; i < songs.length; i++) {
+        text += "<li id=\"" + i + "\">" + songs[i] + "<span><a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\">&#9658;</a></span></li>";
+    }
+    text += "</ul>";
+    document.getElementById("songlist").innerHTML = text;
+    document.getElementById("request").value = "";
+}
+
+function songPlayed(id) {
+    i = id.parentNode.parentNode.id;
+    songs.splice(i,1);
+    text = "<ul class=\"songTitle\">";
+    for (i = 0; i < songs.length; i++) {
+        text += "<li id=\"" + i + "\">" + songs[i] + "<span><a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\">&#9658;</a></span></li>";
+    }
+    text += "</ul>";
+    document.getElementById("songlist").innerHTML = text;
+}
 
