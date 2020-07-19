ec747a31f0f0214573e4530fa4f4be3c8bb239db
FabulousSpaceCat <christinamdeming@gmail.com>
2020-07-19 17:03:52 -0400

    I tried to do a little work to clean up the css but really, it's a goddamn mess.  Page looks good though, I changed the input style to kind of match the list.  Will be adding an event to take input when you hit enter in the text box.

--- tunes.js
+++ tunes.js
@@ -1,24 +1,29 @@
 
 let songs = [];
 
-function listGen() {
+function listGen() { // This way worked fine, but I built a loop to iterate through my array when Javascript already has forEach, which does that for me.
     var text = "<ul>";
     for (let i = 0; i < songs.length; i++) {
-        text += "<a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\"><li id=\"" + i + "\">" + songs[i] + "<span>&#9658;</span></li></a>";
+        text += "<a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + songs[i] + "\" target=\"_blank\"><li class=\"looks\" id=\"" + i + "\">" + songs[i] + "<span>&#9658;</span></li></a>";
     }
     text += "</ul>";
     document.getElementById("songList").innerHTML = text;
 }
 
-function listGen2(item,index) {
-    document.getElementById("theTunes").innerHTML += "<li id=\"" + index + "\"><a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + item + "\" target=\"_blank\">" + item + "<span>&#9658;</span></li></a>";
+function listGen2(item,index) { // The new hotness.  The 
+    document.getElementById("theTunes").innerHTML += "\n<li class=\"looks\" id=\"" + index + "\"><a onclick=\"songPlayed(this)\" href=\"https://www.youtube.com/results?search_query=" + item + "\" target=\"_blank\">" + item + "<span>&#9658;</span></li></a>\n";
 }
 
 function songRequest() {
-    songs.push(document.getElementById("request").value);
-    document.getElementById("theTunes").innerHTML = "";
-    songs.forEach(listGen2);
-    document.getElementById("request").value = "";
+    if (document.getElementById("request").value.trim() != "") { 
+        
+        // This is checking for the input being only spaces or nothing at all - Theoretically, myString && !myString.trim() should have worked, but then it didn't?  According to stackoverflow anyway.  I could also test a regex which looks for at least one character of not whitespace: /\S/.test(string) which might be more compatible with older browsers.  But who's going to be using this in IE8, rly?
+
+        songs.push(document.getElementById("request").value.trim()); // We're trimming leading and trailing spaces before shoving the input into the array
+        document.getElementById("theTunes").innerHTML = ""; // Empty out the <ul>
+        songs.forEach(listGen2); // Generate the list
+        document.getElementById("request").value = ""; // Clear out the text input.
+    }
 }
 
 function songPlayed(id) {
